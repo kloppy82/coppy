5a2efad90fd9f4ffef0974db8a84742a3041dd48
Markus Lempen <markus.lempen@damedics.com>
2018-09-25 09:45:29 +0200

    Removed Output Files

--- pca10056/s140/ses/Output/ble_app_coppy_pca10056_s140 Debug/Obj/ses_startup_nrf52840.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_coppy_pca10056_s140\ Debug/Obj/ses_startup_nrf52840.o: \
- D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\modules\nrfx\mdk\ses_startup_nrf52840.s
--- pca10056/s140/ses/Output/ble_app_coppy_pca10056_s140 Release/Obj/utf.d
+++
@@ -1,6 +0,0 @@
-Output/ble_app_coppy_pca10056_s140\ Release/Obj/utf.o: \
- D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\external\utf_converter\utf.c \
- D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\external\utf_converter\utf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stddef.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/__crossworks.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdint.h
