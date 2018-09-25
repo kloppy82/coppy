13890170f0acf81ffde068866404889d9c378246
Markus Lempen <markus.lempen@damedics.com>
2018-09-25 09:42:06 +0200

    Initial Commit -Basic functionality works

---
+++ pca10056/s140/ses/Output/ble_app_coppy_pca10056_s140 Release/Obj/app_error_handler_gcc.d
@@ -0,0 +1,32 @@
+Output/ble_app_coppy_pca10056_s140\ Release/Obj/app_error_handler_gcc.o: \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\app_error_handler_gcc.c \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\sdk_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/string.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/__crossworks.h \
+ ../../../config/sdk_config.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\nordic_common.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\sdk_os.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/softdevice/s140/headers/nrf_error.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\app_util.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stddef.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\sdk_macros.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\nrf_assert.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdio.h \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\components\libraries\util\app_error_weak.h
---
+++ pca10056/s140/ses/Output/ble_app_coppy_pca10056_s140 Release/Obj/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/ble_app_coppy_pca10056_s140\ Release/Obj/ses_startup_nrf52840.o: \
+ D:\Dropbox\ (NEEO\ AG)\Copperfield\Bluetooth\nRF5_SDK_15.1.0_a8c0c4d\modules\nrfx\mdk\ses_startup_nrf52840.s
